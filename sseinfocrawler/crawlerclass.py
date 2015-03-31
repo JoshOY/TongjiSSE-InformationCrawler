b0b3d1f67481ecead3569b65de1496619bdd5499
Josh Ouyang <JoshOY789@gmail.com>
2015-03-31 23:17:50 +0800

    Add mail functions.

--- sseinfocrawler/crawlerclass.py
+++ sseinfocrawler/crawlerclass.py
@@ -2,7 +2,9 @@
 __author__ = 'JoshOY'
 
 import info_crawler
+import sendmail
 import sys
+import json
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -25,6 +27,23 @@ class SSEInfoCrawler:
     def get_info_detail(url_id):
         return info_crawler.get_info_detail(url_id)
 
+    @staticmethod
+    def send_packaged_mail(info):
+        with open('settings.json') as jsonf:
+            mail_settings = json.loads(jsonf.read())
+            for each_to_send in mail_settings[u'toMail']:
+                sendinfo = sendmail.create_sendinfo(mail_settings[u'smtpHost'],
+                                                    mail_settings[u'fromMail'],
+                                                    each_to_send,
+                                                    mail_settings[u'senderUsername'],
+                                                    mail_settings[u'senderPassword'])
+                mailobj = sendmail.create_mailobj(u'【软院新通知提醒】' + info['title'],
+                                                  info['content'] + u'\n\n发布日期：' + info['date'],
+                                                  mail_settings[u'fromMail'],
+                                                  each_to_send)
+                sendmail.send_mail(sendinfo, mailobj)
+        return True
+
 
 if __name__ == '__main__':
-    print SSEInfoCrawler.search_info_ids()
+    print SSEInfoCrawler.send_packaged_mail(SSEInfoCrawler.get_info_detail(u'1003697'))
--- sseinfocrawler/sendmail.py
+++ sseinfocrawler/sendmail.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 __author__ = 'JoshOY'
 
 import smtplib
@@ -8,3 +9,36 @@ import sys
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
+
+def send_mail(sendinfo, mailobj):
+    encoding = 'utf-8'
+    try:
+        # 初始化邮件，使用SSl
+        smtp = smtplib.SMTP_SSL(sendinfo['smtphost'], sendinfo['sslport'])
+        smtp.ehlo()
+        smtp.login(sendinfo['username'], sendinfo['password'])
+
+        # 发送邮件
+        smtp.sendmail(sendinfo['frommail'], sendinfo['tomail'], mailobj.as_string())
+        smtp.close()
+    except Exception as err:
+        print "Exception: Unable to send e-mail."
+        print err
+
+def create_sendinfo(smtphost, frommail, tomail, username, password, sslport='465'):
+    sendinfo = {}
+    sendinfo['smtphost'] = smtphost
+    sendinfo['sslport'] = sslport
+    sendinfo['frommail'] = frommail
+    sendinfo['tomail'] = tomail
+    sendinfo['username'] = username
+    sendinfo['password'] = password
+    return sendinfo
+
+def create_mailobj(subject, body, frommail, tomail, encoding='utf-8'):
+    mail = MIMEText(body.encode(encoding), 'plain', encoding)
+    mail['Subject'] = Header(subject, encoding)
+    mail['From'] =frommail
+    mail['To'] = tomail
+    mail['Date'] = formatdate()
+    return mail
